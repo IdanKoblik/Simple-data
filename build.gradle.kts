a78fd40bc501c27a02a20ff7ec99c3fbcfcdf789
idank <idankob@gmail.com>
2025-01-11 21:21:56 +0200

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("org.mongodb:mongodb-driver-sync:${findProperty("mongo.driver.version")}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${findProperty("jackson.version")}")
+    implementation("org.jetbrains:annotations:${findProperty("jetbrains.version")}")
+
+    testImplementation(platform("org.junit:junit-bom:${findProperty("junit.version")}"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
